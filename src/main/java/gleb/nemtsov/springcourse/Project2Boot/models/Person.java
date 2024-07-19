3bfca471dd07d74006c4832c42fac7a9f894f4eb
Nemtsov <glebka.nemtsov1611@gmail.com>
2024-07-20 02:24:09 +1000

    initialized my project

---
+++ src/main/java/gleb/nemtsov/springcourse/Project2Boot/models/Person.java
@@ -0,0 +1,70 @@
+package gleb.nemtsov.springcourse.Project2Boot.models;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+import java.util.List;
+
+@Entity
+@Table(name = "Person")
+public class Person {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2, max = 50, message = "Name should be between 2 and 30 characters")
+    @Column(name = "name")
+    private String name;
+
+    @Min(value = 18, message = "Age should be greater than 18")
+    @Column(name = "age")
+    private int age;
+
+    @OneToMany(mappedBy = "owner")
+    private List<Book> books;
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+
+
+    public Person() {
+
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+}
